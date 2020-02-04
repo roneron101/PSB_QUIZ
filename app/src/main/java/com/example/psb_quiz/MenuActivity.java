8a3cbe901533903b7b2ea258fd0ba7cfc9ed7c1a
ron sioson <roneron101@gmail.com>
2020-02-04 18:01:59 +0800

    Sprint 1 - Main Page

--- app/src/main/java/com/example/psb_quiz/MenuActivity.java
+++ app/src/main/java/com/example/psb_quiz/MenuActivity.java
@@ -2,13 +2,31 @@ package com.example.psb_quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    Button ECS, ICOS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        ECS = findViewById(R.id.button3);
+        ICOS = findViewById(R.id.button4);
+
+        ECS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        
+
     }
 }
